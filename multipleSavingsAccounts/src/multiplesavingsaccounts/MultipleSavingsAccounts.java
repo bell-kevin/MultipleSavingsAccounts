eeb9f3ff53cd1c7b1d2a26178b867e5710c601ec
Kevin Bell <bellKevin@pm.me>
2022-06-01 15:40:16 +0000

    Update MultipleSavingsAccounts.java

--- multipleSavingsAccounts/src/multiplesavingsaccounts/MultipleSavingsAccounts.java
+++ multipleSavingsAccounts/src/multiplesavingsaccounts/MultipleSavingsAccounts.java
@@ -10,11 +10,10 @@ public class MultipleSavingsAccounts {
         Account savings1 = new Account();
         Account savings2 = new Account();
         Account savings3 = new Account();
-        int cycles = 0;
-        double interestRate;
-        double balance;
-        int i = 0;
-        //for (i = 0; i < cycles; i++) {
+        int cycles = 1, i;
+        double interestRate, balance;
+        for (i = 1; i < 4; i++) {
+            System.out.println("New Savings Account #" + i);
             System.out.print("Enter starting balance of this Savings Account: ");
             balance = computerKeyboardInput.nextDouble();
             System.out.println("Interest rate: to enter 2.5%, type 2.5");
@@ -22,10 +21,24 @@ public class MultipleSavingsAccounts {
             interestRate = computerKeyboardInput.nextDouble();
             System.out.print("Enter number of cycles to grow this Savings Account: ");
             cycles = computerKeyboardInput.nextInt();
-            savings1.setInterestRate(interestRate);
-            savings1.setBalance(balance);
-            savings1.growAccount(cycles);
-       // }
-        System.out.printf("After %d cycles, the account is worth $%-,10.2f\n", cycles, savings1.getBalance());
+            if (i == 1) {
+                savings1.setInterestRate(interestRate);
+                savings1.setBalance(balance);
+                savings1.growAccount(cycles);
+            } else if (i == 2) {
+                savings2.setInterestRate(interestRate);
+                savings2.setBalance(balance);
+                savings2.growAccount(cycles);
+            } else {
+                savings3.setInterestRate(interestRate);
+                savings3.setBalance(balance);
+                savings3.growAccount(cycles);
+            }
+        }
+        System.out.println("");
+        System.out.printf("Savings Account 1: After growth, the account is worth $%-,10.2f\n", savings1.getBalance());
+        System.out.printf("Savings Account 2: After growth, the account is worth $%-,10.2f\n", savings2.getBalance());
+        System.out.printf("Savings Account 3: After growth, the account is worth $%-,10.2f\n", savings3.getBalance());
+        System.out.println("");
     }
 }
