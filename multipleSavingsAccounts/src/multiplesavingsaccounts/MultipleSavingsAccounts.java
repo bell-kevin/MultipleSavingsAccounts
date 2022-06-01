66ba5c472438166f092d19e521ffe92b6065d25f
Kevin Bell <bellKevin@pm.me>
2022-06-01 10:31:36 -0600

    Update MultipleSavingsAccounts.java

--- multipleSavingsAccounts/src/multiplesavingsaccounts/MultipleSavingsAccounts.java
+++ multipleSavingsAccounts/src/multiplesavingsaccounts/MultipleSavingsAccounts.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MultipleSavingsAccounts {
 
     public static void main(String[] args) {
-        System.out.println("Ch 6 Multiple Accounts by Kevin Bell");
+        System.out.println("Ch 6 Multiple Accounts by Kevin Bell \n");
         Scanner computerKeyboardInput = new Scanner(System.in);
         Account savings1 = new Account();
         Account savings2 = new Account();
@@ -13,7 +13,6 @@ public class MultipleSavingsAccounts {
         int cycles = 1, i;
         double interestRate, balance;
         for (i = 1; i < 4; i++) {
-            System.out.println("");
             System.out.println("New Savings Account #" + i);
             System.out.print("Enter starting balance of this Savings Account: ");
             balance = computerKeyboardInput.nextDouble();
@@ -22,6 +21,7 @@ public class MultipleSavingsAccounts {
             interestRate = computerKeyboardInput.nextDouble();
             System.out.print("Enter number of cycles to grow this Savings Account: ");
             cycles = computerKeyboardInput.nextInt();
+            System.out.print("\n")
             if (i == 1) {
                 savings1.setInterestRate(interestRate);
                 savings1.setBalance(balance);
@@ -36,10 +36,8 @@ public class MultipleSavingsAccounts {
                 savings3.growAccount(cycles);
             }
         }
-        System.out.println("");
         System.out.printf("Savings Account 1: After growth, the account is worth $%-,10.2f\n", savings1.getBalance());
         System.out.printf("Savings Account 2: After growth, the account is worth $%-,10.2f\n", savings2.getBalance());
-        System.out.printf("Savings Account 3: After growth, the account is worth $%-,10.2f\n", savings3.getBalance());
-        System.out.println("");
+        System.out.printf("Savings Account 3: After growth, the account is worth $%-,10.2f\n\n", savings3.getBalance());
     }
 }
